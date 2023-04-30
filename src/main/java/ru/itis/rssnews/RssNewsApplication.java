f8621f923d56da2aa2956bbfb775e90e09ed14e8
Ainur Sabitov <ainursabitovak@mail.ru>
2023-04-30 19:18:08 +0300

    init

---
+++ src/main/java/ru/itis/rssnews/RssNewsApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.rssnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssNewsApplication.class, args);
+    }
+
+}
