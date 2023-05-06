eb120fd99bc65de0308c8895d2f467cb36ff9294
Ainur Sabitov <ainursabitovak@mail.ru>
2023-05-06 22:07:21 +0300

    add models Article and RssSource

---
+++ src/main/java/ru/itis/rssnews/models/Article.java
@@ -0,0 +1,37 @@
+package ru.itis.rssnews.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Entity(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, unique = true)
+    private String link;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private String pubDate;
+
+    @Column(nullable = false)
+    private String category;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_id")
+    private RssSource source;
+}
