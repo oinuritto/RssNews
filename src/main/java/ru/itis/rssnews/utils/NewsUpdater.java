e153e6085801219288f9b3b701d69f18305a56cf
Ainur Sabitov <ainursabitovak@mail.ru>
2023-05-08 19:49:34 +0300

    improved NewsUpdater, deletes not valid sources and shows info about it in logs.

--- src/main/java/ru/itis/rssnews/utils/NewsUpdater.java
+++ src/main/java/ru/itis/rssnews/utils/NewsUpdater.java
@@ -12,6 +12,7 @@ import ru.itis.rssnews.models.RssSource;
 import ru.itis.rssnews.services.ArticlesService;
 import ru.itis.rssnews.services.RssSourcesService;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,12 +29,14 @@ public class NewsUpdater {
     @Scheduled(fixedRate = 2 * 60 * 1000) // Задача выполняется каждые 2 минуты
     public void updateNews() {
         logger.info("Started updating news.");
+        RssSource lastSource = null;
 
         // обновление новостей в БД
         try {
             List<RssSource> sources = rssSourcesService.getAllSources();
 
             for (RssSource source: sources) {
+                lastSource = source; // for catch block of try-catch
                 List<Article> articles = articlesParser.parse(source); // Парсинг новостей из XML-документа
                 // Обновление новостей в БД
                 for (Article article : articles) {
@@ -46,6 +49,11 @@ public class NewsUpdater {
             }
             logger.info("Updating news end.");
 
+        } catch (MalformedURLException ex) {
+            logger.error("Not valid url in rss sources.");
+            logger.info("Deleting this source from db. Its rss url = " + lastSource.getSource());
+            rssSourcesService.deleteById(lastSource.getId());
+            logger.info("Source deleted successfully.");
         } catch (Exception ex) {
             logger.error("Error updating news.", ex);
         }
